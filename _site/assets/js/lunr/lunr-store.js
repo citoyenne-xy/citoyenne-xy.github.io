dd582c33dc608a0a3edb467560b9df423d8620e6
Evrine97 <67479380+Evrine97@users.noreply.github.com>
2021-01-26 00:59:48 +0800

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Index",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/index/",
+        "teaser": "http://localhost:4000/assets/images/teaser.png"
+      },]
